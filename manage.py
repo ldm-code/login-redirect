4eea082525b455eace74a80bf4fb60768384769d
Leonardo Moraes <demoraesleonardo327@gmail.com>
2025-06-17 18:18:28 -0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,8 +5,12 @@ def start():
           return render_template('login.html')
 @app.route('/login',methods=['POST'])
 def menu():
-        nome=request.form['nome']
-        senha=request.form['senha']
-        return render_template('return.html',nome=nome,senha=senha)
+        nome=request.form.get('nome')
+        senha=request.form.get('senha')
+        if not senha or not nome:
+                msg='preencha todos os campos'
+                return render_template('login.html',msg=msg)
+        else:
+          return render_template('return.html',nome=nome,senha=senha)
 if __name__=='__main__':
         app.run(debug=True)
