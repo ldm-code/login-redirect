73010360c5af589ce1196ab9b966bb197a5429c3
Leonardo Moraes <demoraesleonardo327@gmail.com>
2025-06-17 19:06:37 -0300

    comit inicial

--- manage.py
+++ manage.py
@@ -2,12 +2,14 @@ from flask import Flask,request,render_template
 app=Flask(__name__)
 @app.route('/')
 def start():
+
           return render_template('login.html')
 @app.route('/login',methods=['POST'])
 def menu():
         nome=request.form.get('nome')
         senha=request.form.get('senha')
-        if not senha or not nome:
+        qtd_senha=len(senha)
+        if not senha or not nome or qtd_senha<7:
                 msg='preencha todos os campos'
                 return render_template('login.html',msg=msg)
         else:
