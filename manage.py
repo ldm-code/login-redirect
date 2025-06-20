e2a7597bc5b27084f64602665e2392938605cf24
Leonardo Moraes <demoraesleonardo327@gmail.com>
2025-06-20 17:25:48 -0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 from flask import Flask,request,render_template
+from werkzeug.security import generate_password_hash,check_password_hash
 app=Flask(__name__)
 @app.route('/')
 def start():
@@ -9,6 +10,8 @@ def menu():
         nome=request.form.get('nome')
         senha=request.form.get('senha')
         qtd_senha=len(senha)
+        hashed_password=generate_password_hash(senha)
+        check_password_hash(hashed_password,senha)
         if not senha or not nome or qtd_senha>7:
                 msg='preencha todos os campos'
                 return render_template('login.html',msg=msg)
